f5cc14e8c5738e6cb07d4de6a745114694d50d11
feliciaqf <yhhqf@foxmail.com>
2016-08-04 11:06:29 +0800

    add accAdd accSub
    获取浮点数加减法的精确/正确结果

--- utilsCollection.js
+++ utilsCollection.js
@@ -54,7 +54,31 @@
 		}
 		return num;
 	}
-	
+
+        /**
+	 * 用来得到精确的减法结果
+	 */
+        an.accSub = function(arg1, arg2){
+            var r1, r2, m, n; 
+            try { r1 = arg1.toString().split(".")[1].length } catch (e) { r1 = 0 } 
+            try { r2 = arg2.toString().split(".")[1].length } catch (e) { r2 = 0 } 
+            m = Math.pow(10, Math.max(r1, r2)); 
+            n = (r1 >= r2) ? r1 : r2; 
+            return ((arg1 * m - arg2 * m) / m).toFixed(n); 
+        }
+
+        /**
+	 * 用来得到精确的加法结果
+	 */
+        an.accAdd = function(arg1, arg2){
+           var r1, r2, m, n; 
+           try { r1 = arg1.toString().split(".")[1].length } catch (e) { r1 = 0 } 
+           try { r2 = arg2.toString().split(".")[1].length } catch (e) { r2 = 0 } 
+           m = Math.pow(10, Math.max(r1, r2)); 
+           n = (r1 >= r2) ? r1 : r2; 
+           return (arg1 * m + arg2 * m) / m; 
+        }
+
 	/* 地址栏URL操作
 	 ----------------------------------------------------*/
 	utils.AURL = {};
